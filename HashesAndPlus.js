ab903664939294277c24789a8bfc2e7bf807cd13
sofyahoward <sofya.howard@gmail.com>
2019-05-27 14:11:25 -0300

    hashplus

---
+++ HashesAndPlus.js
@@ -0,0 +1,20 @@
+// Create a function that returns the number of hashes and pluses in a string.
+
+// Examples
+// hashPlusCount("###+") ➞ [3, 1]
+
+// hashPlusCount("##+++#") ➞ [3, 3]
+
+// hashPlusCount("#+++#+#++#") ➞ [4, 6]
+
+// hashPlusCount("") ➞ [0, 0]
+
+function hashPlusCount(str) {
+    let obj1 = { hash: 0, plus: 0 };
+    for (const i in str.split('')) {
+        if (str[i] === "#") { obj1.hash++ }
+        else { obj1.plus++ }
+    }
+    return [obj1.hash, obj1.plus]
+}
+hashPlusCount('##+')
